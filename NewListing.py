7dd38ef7def1df6cb3625607424296aa7ffde77e
tulika pandey <tulika1505@gmail.com>
2020-11-02 23:35:25 +0530

    modified v.1

--- NewListing.py
+++ NewListing.py
@@ -1,6 +1,7 @@
 import requests
 import bot
 import pandas as pd
+import time
 from bs4 import BeautifulSoup
 import re
 
@@ -20,33 +21,12 @@ def get_soup(data):
 		print('Error details : {}'.format(e))
 	return soup
 
-def send_userMessage(message):
+def send_userMessage(coin,date,title):
+	message = "Title : {} , Coin Pair : {} , Date : {}".format(title,coin,date)
 	last_msg= bot.retrieve_LastMessage()
 	Userid=last_msg['message']['from']['id']
 	bot.bot_send_message(message, Userid)
 
-
-def check_csv(coin,date,title,removeCsv=None):
-	if len(coin) != 0 :
-		new_df= pd.read_csv("New-CoinPair.csv")
-		remove_df = pd.read_csv("Remove-CoinPair.csv")
-		coin = " ".join(coin)
-		message = "Title : {} , Coin Pair : {} , Date : {}".format(title,coin,date)
-		if removeCsv is not None:
-			#check remove coin pair csv
-			if coin not in list(remove_df['CoinPair']):
-				#send msg to bot and add to remove csv
-				send_userMessage(message)
-				add_data = pd.DataFrame({'CoinPair' : [coin], 'Date' : [date]})
-				add_data.to_csv('Remove-CoinPair.csv' , mode='a',header=False ,index = False)
-
-		else:
-			#check add coin pair csv 
-			if coin not in list(new_df['CoinPair']):
-				#send msg to bot and add to csv
-				send_userMessage(message)
-				add_data = pd.DataFrame({'CoinPair' : [coin], 'Date' : [date]})
-				add_data.to_csv('New-CoinPair.csv' , mode='a',header=False ,index = False)
 
 def find_date_time(text,remove=None):
 	if text is not None:
@@ -59,31 +39,30 @@ while True:
 	listingData = get_request("/en/support/announcement/c-48")
 	soup = get_soup(listingData)
 	allLists = soup.find_all('a',class_='css-1neg3js')
-
+	link_df = pd.read_csv("HrefLinks.csv")
 	if allLists is not None:
 		for listdata in allLists:
+			data=None
 			title=listdata.text
-			if "binance will list" in listdata.text.lower():
-				listDetails = get_request(listdata['href'])
-				soup = get_soup(listDetails)
-				data = str(soup.find('article',class_='css-1ii68zy'))
-				date,coin = find_date_time(data)
-				check_csv(coin,date,title)
-				print(coin,date)
-			elif "binance lists" in listdata.text.lower():
-				listDetails = get_request(listdata['href'])
-				soup = get_soup(listDetails)
-				data = soup.find_all("div",class_='css-3fpgoh')[1].text
-				date,coin = find_date_time(data)
-				check_csv(coin,date,title)
-				print(coin,date)
-			elif "Notice of Removal" in listdata.text:
-				listDetails=get_request(listdata['href'])
-				soup = get_soup(listDetails)
-				data = str(soup.find('article',class_='css-1ii68zy'))
-				date,coin = find_date_time(data)
-				check_csv(coin,date,title,"remove")
-				print(coin,date)
-				break
-			else:
-				pass
+			link= str(listdata['href'])
+			if link not in list(link_df['Links']):
+				add_data = pd.DataFrame({'Links' : [link]})
+				add_data.to_csv('HrefLinks.csv' , mode='a',header=False ,index = False)
+				listDetails = get_request(link)
+				if "binance will list" in listdata.text.lower():
+					soup = get_soup(listDetails)
+					data = str(soup.find('article',class_='css-1ii68zy'))
+				elif "binance lists" in listdata.text.lower():
+					soup = get_soup(listDetails)
+					data = soup.find_all("div",class_='css-3fpgoh')[1].text
+				elif "Notice of Removal" in listdata.text:
+					soup = get_soup(listDetails)
+					data = str(soup.find('article',class_='css-1ii68zy'))
+					removeBool = True
+				else:
+					pass
+				if data is not None:
+					date,coin = find_date_time(data)
+					send_userMessage(" ".join(coin),date,title)
+				
+	time.sleep(30)
